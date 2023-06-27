70b82c34946031b0af61bc1e5db50646d0d36e91
POURIA <p.khadivar@gmail.com>
2023-06-27 17:46:58 +0430

    Initialaize the project and folder structure

---
+++ app/(authentication)/getting-started/page.tsx
@@ -0,0 +1,9 @@
+const GettingStarted = () => {
+  return (
+    <div>
+      <h1>Getting Started</h1>
+    </div>
+  )
+}
+
+export default GettingStarted
---
+++ app/(authentication)/single-sign-on/page.tsx
@@ -0,0 +1,9 @@
+const SingleSignOn = () => {
+    return (
+        <div>
+         <h1>Single Sign On</h1>
+        </div>
+    )
+}
+
+export default SingleSignOn
